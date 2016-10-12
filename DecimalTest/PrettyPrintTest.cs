35608073c168060f3199ccc1001e65635cf29ff4
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 13:37:54 +0200

    Changed name of unit test.

--- DecimalTest/PrettyPrintTest.cs
+++ DecimalTest/PrettyPrintTest.cs
@@ -7,7 +7,7 @@ namespace DecimalTest {
     [TestClass]
     public class PrettyPrintTest {
         [TestMethod]
-        public void TestMethod1() {
+        public void TestPrettyPrint() {
             var inputs = new List<int> { 1, 10, 100, 1000, 10000, 100000, 1000000, 35235235 };
             var result = new List<string>();
             var expected = new List<string> {"1", "10","100", "1,000", "10,000", "100,000", "1,000,000", "35,235,235" };
