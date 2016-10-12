b0a4c58aed85419fccd5cfd77ca50b121e4569a2
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 12:51:26 +0200

    Added some additional classes.

--- DecimalPrettyPrint/Helpers/CharListExtensions.cs
+++ DecimalPrettyPrint/Helpers/CharListExtensions.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace DecimalPrettyPrint
-{
-    public class Class1
-    {
+namespace DecimalPrettyPrint.Helpers {
+    public static class CharListExtensions {
     }
 }
---
+++ DecimalPrettyPrint/customDecimal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DecimalPrettyPrint.Helpers;
+
+namespace DecimalPrettyPrint {
+    public static class customDecimal { //extension class for handling custom decimal modifications
+
+    }
+}
