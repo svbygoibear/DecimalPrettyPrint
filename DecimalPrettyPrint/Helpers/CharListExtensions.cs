36f698d1c218e60c21b4710f47ba708d097c0c3f
simoneslatnpou <svbyogibear@gmail.com>
2016-10-12 13:37:19 +0200

    Finished up pretty print method and unit test.

--- DecimalPrettyPrint/Helpers/CharListExtensions.cs
+++ DecimalPrettyPrint/Helpers/CharListExtensions.cs
@@ -5,5 +5,33 @@ using System.Text;
 
 namespace DecimalPrettyPrint.Helpers {
     public static class CharListExtensions {
+        /// <summary>
+        /// Flattens a list of char to a single string.
+        /// </summary>
+        /// <param name="charItems">List of char</param>
+        /// <returns>Single string value</returns>
+        public static string flatten(this List<char> charItems) {
+            var flattenedString = new StringBuilder();
+            charItems.ForEach(item => {
+                flattenedString.Append(item);
+            });
+            return flattenedString.ToString();
+        }
+
+        /// <summary>
+        /// Gets a subset from a list, starting from the left, of char and returns it as a string.
+        /// </summary>
+        /// <param name="values">List of char</param>
+        /// <param name="curr">Amount of characters to return</param>
+        /// <param name="res">String to add result to</param>
+        /// <returns>String from char list</returns>
+        public static string getCharSubset(this List<char> values, int curr, string res) {
+            if (curr == 0)
+                return res;
+
+            var additional = values[values.Count - 1];
+            values.RemoveAt(values.Count - 1);
+            return getCharSubset(values, curr - 1, res = $"{additional}{res}");
+        }
     }
 }
---
+++ DecimalTest/PrettyPrintTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using DecimalPrettyPrint;
+
+namespace DecimalTest {
+    [TestClass]
+    public class PrettyPrintTest {
+        [TestMethod]
+        public void TestMethod1() {
+            var inputs = new List<int> { 1, 10, 100, 1000, 10000, 100000, 1000000, 35235235 };
+            var result = new List<string>();
+            var expected = new List<string> {"1", "10","100", "1,000", "10,000", "100,000", "1,000,000", "35,235,235" };
+
+            inputs.ForEach(input => {
+                result.Add(input.decimalPrettyPrint());
+            });
+        }
+    }
+}
